b5e2f634a286657ea4b622cb8b08e90846a6a3d3
Lorenz Herzberger <lorenzherzberger@gmail.com>
2024-04-04 10:22:31 +0200

    fix: for loop on gauge registration
    Signed-off-by: Lorenz Herzberger <lorenzherzberger@gmail.com>

--- cmd/rddl-prometheus-exporter/main.go
+++ cmd/rddl-prometheus-exporter/main.go
@@ -8,8 +8,7 @@ import (
 	"github.com/prometheus/client_golang/prometheus"
 	"github.com/prometheus/client_golang/prometheus/promhttp"
 	"github.com/rddl-network/rddl-prometheus-exporter/config"
-
-	elements "github.com/rddl-network/elements-rpc"
+	"github.com/rddl-network/rddl-prometheus-exporter/elements"
 )
 
 func setGauge(name string, help string, namespace string, subsystem string, callback func() float64) {
@@ -31,18 +30,14 @@ func main() {
 	logger := log.Default()
 
 	for _, wallet := range strings.Split(cfg.Wallets, ",") {
-		wallet = strings.TrimSpace(wallet)
+		wallet := strings.TrimSpace(wallet)
 		sanitizedWallet := strings.ReplaceAll(wallet, "-", "_")
 		logger.Printf("registering gauge for wallet: " + wallet)
 		setGauge("balance_"+sanitizedWallet, "Bitcoin balance for network relevant wallet: "+wallet, "elementsd", "wallets", func() float64 {
-			res, err := elements.GetBalance(cfg.GetElementsURL(wallet), []string{})
+			url := cfg.GetElementsURL(wallet)
+			balance, err := elements.GetWalletBalance(url, wallet)
 			if err != nil {
-				logger.Printf("error getting balance for wallet %s: %s", wallet, err)
-				return 0
-			}
-			balance, ok := res["bitcoin"]
-			if !ok {
-				logger.Printf("bitcoin balance not found for wallet %s", wallet)
+				log.Print(err.Error())
 				return 0
 			}
 			return balance
