1abdc693ac217d24c5bc7f84b25ad3da25edcda2
Lorenz Herzberger <lorenzherzberger@gmail.com>
2024-04-04 10:26:41 +0200

    fix: linter errros
    Signed-off-by: Lorenz Herzberger <lorenzherzberger@gmail.com>

--- elements/wallet.go
+++ elements/wallet.go
@@ -1,6 +1,7 @@
 package elements
 
 import (
+	"errors"
 	"fmt"
 
 	elements "github.com/rddl-network/elements-rpc"
@@ -10,7 +11,7 @@ import (
 func GetWalletBalance(url string, wallet string) (balance float64, err error) {
 	res, err := elements.GetBalance(url, []string{})
 	if err != nil {
-		return 0, fmt.Errorf("error getting balance for wallet %s: %s", wallet, err)
+		return 0, fmt.Errorf("error getting balance for wallet %s: %w", wallet, err)
 	}
 	balance, err = GetAsset(res)
 	if err != nil {
@@ -22,7 +23,7 @@ func GetWalletBalance(url string, wallet string) (balance float64, err error) {
 func GetAsset(balance types.GetBalanceResult) (float64, error) {
 	btc, ok := balance["bitcoin"]
 	if !ok {
-		return 0, fmt.Errorf("bitcoin balance not found")
+		return 0, errors.New("bitcoin balance not found")
 	}
 	return btc, nil
 }
